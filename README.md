8214f7f42ea11f4af6ac36bcf8cc20c8ecc0725d
Daniel <daniel.sierra@3pillarglobal.com>
2023-08-14 14:21:32 -0600

    Connected TypeORM with postgres via app.module

--- README.md
+++ README.md
@@ -3,9 +3,24 @@
 
 # Tesla API
 
-1. Build database
+1. Clone repo
+
+2. Install dependencies
+
+```
+  yarn install
+```
+
+3. Build database
 
 ```
   docker-compose up -d
 ```
+
+4. Run dev mode
+```
+  yarn start:dev
+```
+
+
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
-})
+imports: [
+  ConfigModule.forRoot(), // this is for using .env variables
+
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST, // this is an .env file
+    port: +process.env.DB_PORT, // this is an .env file
+    database: process.env.DB_NAME, // this is an .env file
+    username: process.env.DB_USERNAME, // this is an .env file
+    password: process.env.DB_PASSWORD, // this is an .env file
+    autoLoadEntities: true,
+    synchronize: true, // this should be false in prod
+  })
+
+], })
+
 export class AppModule {}
